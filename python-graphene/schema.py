e9c6f00f5cfd1a1dc59bbe9e5a99e44e827d891e
Josh Wood <wood2jp2@gmail.com>
2019-05-30 08:28:12 -0400

    through lecture 8, added User schema as well as resolver function to retrieve list of users

--- python-graphene/schema.py
+++ python-graphene/schema.py
@@ -1,10 +1,17 @@
 import graphene
 import json
+from datetime import datetime
+
+class User(graphene.ObjectType):
+    id = graphene.ID()
+    username = graphene.String()
+    created_at = graphene.DateTime()
 
 class Query(graphene.ObjectType):
     hello = graphene.String()
     is_admin = graphene.Boolean()
     is_wack = graphene.Boolean()
+    users = graphene.List(User, limit=graphene.Int())
 
     def resolve_hello(self, info):
         return "world"
@@ -15,12 +22,21 @@ class Query(graphene.ObjectType):
     def resolve_is_wack(self, info):
         return False
 
+    def resolve_users(self, info, limit=None):
+        return [
+            User(id="1", username="Mark", created_at=datetime.now()),
+            User(id="2", username="Josh", created_at=datetime.now())
+
+        ][:limit]
 schema = graphene.Schema(query=Query)
 
 result = schema.execute(
     '''
     {
-        isWack
+        users(limit: 1) {
+            id,
+            username
+        }
     }
     '''
 )
